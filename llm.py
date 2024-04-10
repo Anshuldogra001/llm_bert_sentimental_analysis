7d381ffe14704839237a8919fe8fcf6b055ef556
Abhinav Dogra <96309140+Anshuldogra001@users.noreply.github.com>
2024-04-10 22:03:56 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -9,6 +9,30 @@ import speech_recognition as sr
 from transformers import BartForConditionalGeneration, BartTokenizer
 import streamlit as st
 import base64
+
+
+
+
+
+import gdown
+import os
+
+
+def download_file_from_drive(destination_folder):
+    download_link = 'https://drive.google.com/uc?id=1s_Ps2DRwG9x635H0b1oX1a6puP_iIazM'
+    destination_path = os.path.join(destination_folder, f'pytorch_model.bin') 
+    gdown.download(download_link, destination_path, quiet=False)
+
+    print(f"File downloaded to {destination_path}")
+destination_folder = 'saved_model'
+
+download_file_from_drive(destination_folder)
+
+
+
+
+
+
 st.title("It's Sentiment Analysis!")
 
 # uncomment it only if the other file not working
